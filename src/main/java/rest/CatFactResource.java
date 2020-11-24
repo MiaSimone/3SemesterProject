b9fbdabb2f63db091493ef9d5fde1645cc215b07
Mia Simone <cph-md291@cphbusiness.dk>
2020-11-24 18:24:01 +0100

    Changed catresource

--- src/main/java/rest/CatFactResource.java
+++ src/main/java/rest/CatFactResource.java
@@ -3,6 +3,9 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 import dto.CatFactDTO;
 import fetchers.CatFactFetcher;
 import java.io.IOException;
@@ -31,6 +34,43 @@ public class CatFactResource {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     private static ExecutorService es = Executors.newCachedThreadPool();
     private static String cachedResponse;
+    
+    
+    @GET
+    @Path("characters")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getCharacters() throws Exception {
+        /*
+        URL url = new URL("https://realtor.p.rapidapi.com/properties/v2/list-for-sale?city=New%20York%20City&limit=1&offset=0&state_code=NY&sort=relevance");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestProperty("x-rapidapi-key", "d12379afb6msh4e1abe29e50bca0p15e49ejsn56af46069613");
+        con.setRequestProperty("x-rapidapi-host", "realtor.p.rapidapi.com");
+        */
+        
+        /*
+        HttpResponse<JsonNode> response = Unirest.get("https://realtor.p.rapidapi.com/properties/v2/list-for-sale?city=New%20York%20City&limit=1&offset=0&state_code=NY&sort=relevance")
+	.header("x-rapidapi-key", "d12379afb6msh4e1abe29e50bca0p15e49ejsn56af46069613")
+	.header("x-rapidapi-host", "realtor.p.rapidapi.com")
+	.asJson();
+        
+        JSONObject myObject = response.getBody().getObject();
+        
+        System.out.println(myObject);
+        //return GSON.toJson(response.getBody());
+        */
+        
+        HttpResponse<JsonNode> response = Unirest.get("https://realtor.p.rapidapi.com/properties/v2/list-for-sale?city=New%20York%20City&limit=1&offset=0&state_code=NY&sort=relevance")
+	.header("x-rapidapi-key", "d12379afb6msh4e1abe29e50bca0p15e49ejsn56af46069613")
+	.header("x-rapidapi-host", "realtor.p.rapidapi.com")
+	.asJson();
+        
+        Gson gson = new Gson();
+        String responseJSONString = response.getBody().toString();
+        
+        return responseJSONString;
+    }
     
     
     @GET
